d0e2a9e3725dea9fd3a4640610506fe5e40dac26
Maneki1 <79747047+Maneki1@users.noreply.github.com>
2021-02-27 19:43:40 +0700

    Add files via upload

--- src/util/getColor.ts
+++ src/util/getColor.ts
@@ -1,1 +1,8 @@
+import { DefaultTheme } from "styled-components";
+import getThemeValue from "./getThemeValue";
 
+const getColor = (color: string, theme: DefaultTheme): string => {
+  return getThemeValue(`colors.${color}`, color)(theme);
+};
+
+export default getColor;
